4ca71bc9d6cc6f1ee21e6230861924fb7dd25b6a
Quang Minh <157796411+minhminh12315@users.noreply.github.com>
2024-09-14 20:40:44 +0700

    first commit

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        return 'login';
+    }
+
+    public function logout(Request $request)
+    {
+        return 'logout';
+    }
+}
---
+++ backend/app/Http/Controllers/MedicineController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Medicine;
+use App\Http\Requests\StoreMedicineRequest;
+use App\Http\Requests\UpdateMedicineRequest;
+use Illuminate\Http\Request;
+
+class MedicineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Medicine::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    // public function store(StoreMedicineRequest $request)
+    // {
+    //     //
+    // }
+
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $medicine = Medicine::create($fields);
+
+        return $medicine;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Medicine $medicine)
+    {
+        return $medicine;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Medicine $medicine)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+
+        $medicine->update($fields);
+
+        return $medicine;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Medicine $medicine)
+    {
+        $medicine->delete();
+
+        return ['message' => 'Medicine deleted'];
+    }
+}
